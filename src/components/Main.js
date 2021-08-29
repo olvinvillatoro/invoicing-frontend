a8d6328aa8c4b8abbaa7ac420afbe2f893155d21
= <=>
2021-08-29 02:19:32 -0600

    fully woriking pagination

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,32 +3,39 @@ import Cart from './Cart'
 import Product from './Product';
 import { useState, useEffect } from 'react';
 import Products from './Products'
+import ReactPaginate from 'react-paginate';
 
 const Main = (props) => {
     const [pageNumber,setPageNumber] = useState(0);
-    const productPerPage=10;
+    const productPerPage=3;
     const pagesVisited = pageNumber*productPerPage
     const {products,addItem} = props;
 
-
+    const pageCount = Math.ceil(products.length/ productPerPage);
     const displayProducts = products.slice(pagesVisited, pagesVisited+productPerPage).map((product =>{
         console.log("product--------->",product)
         return(
-            <div className="grid">
-           
+            <div className="row">
               <Product addItem={addItem} key={product.id} product={product}/>
-              
-            
-            
-            
-        </div>
-        );
-    }
+            </div>
+            );}
+        
         
         ));
+    const pageChange = ({selected})=>{
+            setPageNumber(selected)
+    }
     return (
         <main className="col">
           {displayProducts}
+          <ReactPaginate 
+          previousLabel={"Anterior"}
+          nextLabel={"Siguiente"}
+          pageCount={pageCount}
+          onPageChange={pageChange}
+          containerClassName={"paginationButtons"}
+          activeClassName={"activePage"}
+            />
         </main>
 
     )
--- src/components/Product.js
+++ src/components/Product.js
@@ -6,13 +6,13 @@ const Product = (props) => {
         console.log("click en botton",addItem);
     }
     return (
-            <div className="g-col-6 g-col-md-4">
+            <div className="col">
                 <div className="card" style={{width : '200px'}}>
                     <img src={product.img} className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">{product.name}</h5>
                             <p className="card-text">{product.descr}</p>
-                            <button onClick={()=>addItem(product)} className="btn btn-primary">Add to cart</button>
+                            <button onClick={()=>addItem(product)} className="btn btn-primary">Agregar al carrito</button>
                         </div>
                 </div>
             </div>
